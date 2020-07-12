24e21cb097df9dc6b0544366392f5ae886bdc5e2
malwareslayer <alternate-egoist@protonmail.com>
2020-07-12 20:44:56 +0700

    Updating interface using json rather than using argument

--- securimage.py
+++ securimage.py
@@ -5,55 +5,64 @@ Created by Cyber Security Diskominfo Kota Serang (https://cybersec.serangkota.go
 - malwareslayer
 - ahdihaikal
 - gagaltotal
-
+- Sorong6etar
 Dinas Komunikasi Dan Informatika Kota Serang (https://kominfo.serangkota.go.id)
 """
 
 import asyncio
-import pytesseract
 import re
 import os
 import sys
+import json
+import pytesseract
 
-from itertools import count
 from aiohttp import ClientSession
+from typing import (
+    AnyStr,
+    Dict,
+    Optional
+)
 
 from lib.core.enums import PRIORITY
-from lib.core.data import logger, conf
+from lib.core.data import logger
 
 
 class OCRTamper:
-    default_image_path = "/tmp/captcha.png"
-    default_headers_path = "/tmp/headers"
+    __default_image_path = "/tmp/captcha.png"
+    __default_headers_path = "/tmp/headers"
+    __results = None
 
-    def __init__(self, image_url, captcha_params, conf_headers, mode, tesseract_config):
-        self.image_url = image_url
-        self.captcha_params = captcha_params
-        self.conf_headers = conf_headers
-        self.tesseract_config = tesseract_config
-        self.mode = mode
-        self._results = None
+    def __init__(self, data: json.loads):
+        self.images: AnyStr = data["images"]
+        self.parameters: AnyStr = data["parameters"]
+        self.headers: Dict[AnyStr] = data["headers"]
+        self.mode: AnyStr = data["mode"]
+
+        try:
+            self.config: Optional[Dict[AnyStr]] = data["config"]
+        except json.decoder.JSONDecodeError:
+            logger.debug("Something error in config, ignore")
+            self.config: Optional[Dict[AnyStr]] = None
 
         asyncio.get_event_loop().run_until_complete(self.run())
 
     def __del__(self):
-        os.remove(self.default_image_path)
+        os.remove(self.__default_image_path)
 
     async def run(self):
         await self.downloadimage()
-
         if self.mode == 'math':
-            expressions = await self.tesseract
-            self._results = await self.calculate(expressions)
+            expressions = await self.tesseractloads
+            self.__results = await self.calculate(expressions)
         elif self.mode == 'character':
-            self._results = await self.preview
+            self.__results = await self.preview
         elif self.mode == 'easy':
-            self._results = await self.tesseract
+            self.__results = await self.tesseractloads
         else:
             logger.critical("input either math, character or easy")
             exit(1)
 
-    async def calculate(self, expressions):
+    async def calculate(self, expressions) -> AnyStr:
         expressions = re.search(r'\d+[-+x*/]\d+', expressions)
 
         try:
@@ -68,28 +77,27 @@ class OCRTamper:
             expressions[1] = '//'
 
         expressions = ''.join(expressions)
-
         results = str(eval(expressions))
 
         logger.info(f"{expressions} = {results}")
 
-        return "&" + self.captcha_params + "=" + results
+        return "&" + self.parameters + "=" + results
 
-    async def downloadimage(self):
-        async with ClientSession(headers=self.conf_headers) as Session:
-            async with Session.get(self.image_url) as Response:
+    async def downloadimage(self) -> None:
+        async with ClientSession(headers=self.headers) as Session:
+            async with Session.get(self.images) as Response:
                 image = await Response.read()
-                with open(self.default_image_path, "wb+") as file:
+                with open(self.__default_image_path, "wb+") as file:
                     file.write(image)
 
     @property
-    def result(self):
-        return self._results
+    def result(self) -> AnyStr:
+        return self.__results
 
     @property
-    async def preview(self):
+    async def preview(self) -> AnyStr:
         catimg = await asyncio.create_subprocess_shell(
-            f"catimg {self.default_image_path}",
+            f"catimg {self.__default_image_path}",
             stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
 
         stdout, stderr = await catimg.communicate()
@@ -97,58 +105,77 @@ class OCRTamper:
         if stderr:
             raise stderr
         else:
-            return "&" + self.captcha_params + "=" + input("Real Captcha ? : ")
+            return "&" + self.parameters + "=" + input("Real Captcha ? : ")
 
     @property
-    async def tesseract(self):
+    async def tesseractloads(self) -> pytesseract.image_to_string:
+
+        try:
+            if self.config is None:
+                convert = "-colorspace Gray -blur 3x1 -morphology Erode Diamond:1x4 -level 20%"
+            else:
+                convert = self.config["convert"]
+        except KeyError:
+            convert = "-colorspace Gray -blur 3x1 -morphology Erode Diamond:1x4 -level 20%"
+
         imagick = await asyncio.create_subprocess_shell(
             f"""
-            convert {self.default_image_path} \
-            -colorspace Gray                  \
-            -blur 3x1                         \
-            -morphology Erode Diamond:1x4     \
-            -level 20%                        \
-            {self.default_image_path}
+            convert {self.__default_image_path} \
+            {convert}                           \
+            {self.__default_image_path}
             """,
             stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
 
         stdout, stderr = await imagick.communicate()
 
+        try:
+            if self.config is None:
+                tesseract = None
+            else:
+                tesseract = self.config["tesseract"]
+        except KeyError:
+            tesseract = None
+
         if stderr:
             raise stderr
         else:
-            return pytesseract.image_to_string(self.default_image_path, config=self.tesseract_config)
+            return pytesseract.image_to_string(self.__default_image_path, config=tesseract)
 
 
 __priority__ = PRIORITY.HIGHEST
 
 baseargv = sys.argv.index(os.path.basename(__file__).split(".")[0])
 
+filejsonpath: str = ""
+filejson: str = ""
+datajson: json.loads = None
+
 try:
-    urlpath = sys.argv[baseargv + 1]
-    captchaparams = sys.argv[baseargv + 2]
-    cookie = sys.argv[baseargv + 3]
+    filejsonpath: str = sys.argv[baseargv + 1]
 except IndexError:
-    logger.critical("Please input argument into tamper or make --tamper securimagecaptcha in last argument")
+    logger.critical("Please input argument json file into tamper or make --tamper securimagecaptcha in last argument")
+    exit(1)
+
+logger.debug("Load: " + filejsonpath)
+
+try:
+    filejson = open(filejsonpath, "r+").read()
+except FileNotFoundError:
+    logger.critical("Cannot find the path you input")
     exit(1)
 
 try:
-    tesseract_config = sys.argv[baseargv + 4]
-except IndexError:
-    tesseract_config = None
-
-mode = input("Captcha Math Mode ? [math/character/easy] ")
-
+    datajson = json.loads(filejson)
+except json.decoder.JSONDecodeError:
+    logger.critical("Check your json file")
+    exit(1)
 
 def depedencies():
     pass
 
 
 def tamper(payload, **kwargs):
-    headers = {
-        "Cookie": cookie
-    }
 
-    ocr = OCRTamper(urlpath, captchaparams, headers, mode, tesseract_config=tesseract_config)
+    ocr = OCRTamper(datajson)
 
     return payload + ocr.result
